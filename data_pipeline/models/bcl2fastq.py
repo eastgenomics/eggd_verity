714566f0b3fcf487c5d8dba12c047268a1ca9fa4
pofuoku <princeofuoku@gmail.com>
2025-09-10 16:55:56 +0100

    remove fastqbylane

--- data_pipeline/models/bcl2fastq.py
+++ data_pipeline/models/bcl2fastq.py
@@ -31,24 +31,3 @@ class Bcl2fastqBySample(BaseMetrics, table=True):
     percent_perfectindex: float | None = Field(default=None)
     mean_qscore: float | None = Field(default=None)
 
-## TODO
-## This is a run level metric; current pipeline wont work as there are no samples
-# class Bcl2fastqByLane(BaseMetrics, table=True):
-#     """Metrics from the bcl2fastq tool, aggregated by lane."""
-
-#     __tablename__ = "bcl2fastq_bylane"
-#     multiqc_section_names: ClassVar[list[str]] = ["multiqc_bcl2fastq_bylane"]
-
-#     # Relationship back to the parent Sample
-#     sample: "Sample" = Relationship(back_populates="bcl2fastq_bylane_metrics")
-
-#     total: int
-#     total_yield: int
-#     perfectindex: int
-#     undetermined: int
-#     yieldq30: int
-#     qscore_sum: int
-#     percent_q30: float
-#     percent_perfectindex: float
-#     mean_qscore: float
-#     unknown_barcodes: str # remove this
